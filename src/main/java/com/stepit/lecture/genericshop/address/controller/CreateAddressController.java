c35e42ae4537b680034cae9c4974313bf320bb0f
Artem <artemiy.jur@gmail.com>
2024-05-03 20:51:06 +0300

    Added Spring Security.

--- src/main/java/com/stepit/lecture/genericshop/address/controller/CreateAddressController.java
+++ src/main/java/com/stepit/lecture/genericshop/address/controller/CreateAddressController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class CreateAddressController {
     @SuppressWarnings("unused")
     @PostMapping("${app.api.path.address.createAddress}")
     public ResponseEntity<AddressDto> createAddress(
-            @RequestBody CreateAddressRequest createAddressRequest
+            @RequestBody @Validated CreateAddressRequest createAddressRequest
     ) {
         AddressDto address = addressService.createAddress(createAddressRequest);
         return new ResponseEntity<>(address, HttpStatus.OK);
--- src/main/java/com/stepit/lecture/genericshop/address/request/CreateAddressRequest.java
+++ src/main/java/com/stepit/lecture/genericshop/address/request/CreateAddressRequest.java
@@ -1,5 +1,9 @@
 package com.stepit.lecture.genericshop.address.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,8 +11,16 @@ import lombok.Data;
 @Builder
 public class CreateAddressRequest {
 
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]{3,20}$",
+            message = "Field 'City' should be between 3 and 20 characters long and can contains only latin letters.")
     private String city;
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]{3,20}$",
+            message = "Field 'Street' should be between 3 and 20 characters long and can contains only latin letters.")
     private String street;
+    @Positive
+    @Max(value = 1000, message = "street num should be less than 1000")
     private Double streetnum;
 
 }
